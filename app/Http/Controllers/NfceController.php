7c36c352564d621540953a1809d83ced7a5479b9
Jeferson Maciel <ti@ebitsistemas.com.br>
2024-10-19 18:17:17 -0300

    Filesystem

--- app/Http/Controllers/NfceController.php
+++ app/Http/Controllers/NfceController.php
@@ -72,7 +72,6 @@ class NfceController extends Controller
 
     public function consultar(Request $request)
     {
-        dd($request);
         $notafiscal = NotaFiscalService::prepararNfe($request->id, 65);
         $consulta = NfceService::consultar($notafiscal);
 
