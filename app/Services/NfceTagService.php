d185a379b6b1124355b3fa5f8f4baaddc3cafcaf
Jeferson Maciel <ti@ebitsistemas.com.br>
2024-11-26 19:47:59 -0300

    Filesystem

--- app/Services/NfceTagService.php
+++ app/Services/NfceTagService.php
@@ -176,7 +176,7 @@ class NfceTagService
 
     public static function icms($cont, $nfe, $item)
     {
-        dd($item['cProd']);
+        dd($item);
         $tributacaoProduto = TributacaoProduto::with('tributacao')
             ->where('produto_id', $item->cProd)
             ->first();
