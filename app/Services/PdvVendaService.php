88056ea4d9a82934e365171e16919b4f0235362e
Jeferson Maciel <ti@ebitsistemas.com.br>
2025-01-06 19:20:19 -0300

    Filesystem

--- app/Services/PdvVendaService.php
+++ app/Services/PdvVendaService.php
@@ -327,11 +327,12 @@ class PdvVendaService
                 }
 
                 $xml = NfceService::gerarNfce($notafiscal);
-                print_r($xml); exit;
 
                 if (!$xml->tem_erro) {
                     $retorno->chave = $xml->chave;
                     $xml_assinado = NfceService::assinarXml($xml->xml, $xml->chave, $notafiscal);
+                    print_r($xml->chave);
+                    print_r($xml_assinado); exit;
                     if (!$xml_assinado->tem_erro) {
                         $envio = NfceService::enviarXML($xml_assinado->xml, $xml->chave, $notafiscal);
                         if (!$envio->tem_erro) {
