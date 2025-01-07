7c1921c3b4760499a2eebb4cf68c0218392d5a43
Jeferson Maciel <ti@ebitsistemas.com.br>
2025-01-07 19:36:34 -0300

    Filesystem

--- app/Models/Tributacao.php
+++ app/Models/Tributacao.php
@@ -97,8 +97,6 @@ class Tributacao extends Model
             $tributacaoSelecionada->cfop = $produto->cfop;
         }
 
-        print_r($tributacaoSelecionada); exit;
-
         return $tributacaoSelecionada;
     }
 }
