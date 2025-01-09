84417cdf9107c485b9733991fb123a846856415e
Jeferson Maciel <ti@ebitsistemas.com.br>
2025-01-09 19:09:01 -0300

    Filesystem

--- app/Services/PdvVendaService.php
+++ app/Services/PdvVendaService.php
@@ -310,7 +310,6 @@ class PdvVendaService
             if ($nfce) {
                 $retorno->nfce_id = $nfce->id;
                 $notafiscal = NotaFiscalService::prepararNfce($nfce);
-                //print_r($notafiscal); exit;
 
                 if ($nfce->status_id == config('constantes.status.EM_PROCESSAMENTO')) {
                     return $retorno;
